286b243f4cd0e36c537336ce7249554fc146da26
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-08-17 15:53:54 -0500

    fix morphs

--- src/main/java/com/divinity/hmedia/rgrbillionaire/event/ClientModEvents.java
+++ src/main/java/com/divinity/hmedia/rgrbillionaire/event/ClientModEvents.java
@@ -270,7 +270,12 @@ public class ClientModEvents {
     }
 
     private static <T extends IHasGeoRenderer & GeoAnimatable> void createSimpleMorphRenderer(Morph morph, String name, T animatable, float scale) {
-        MorphRenderers.registerPlayerMorphRenderer(morph, context -> new AdvancedGeoPlayerRenderer<>(context, new AdvancedGeoPlayerModel<>(RGRBillionaire.MODID, name), animatable, scale) {
+        MorphRenderers.registerPlayerMorphRenderer(morph, context -> new AdvancedGeoPlayerRenderer<>(context, new AdvancedGeoPlayerModel<>(RGRBillionaire.MODID, name){
+            @Override
+            public ResourceLocation getTextureResource(T animatable, @Nullable AbstractClientPlayer player) {
+                return new ResourceLocation(RGRBillionaire.MODID, "textures/entity/" + name + ".png");
+            }
+        }, animatable, scale) {
             @Override
             public void preRender(PoseStack poseStack, T animatable1, BakedGeoModel model, @Nullable MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
                 super.preRender(poseStack, animatable1, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
