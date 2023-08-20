2e32433fff1cec6c000230551f903b40c9a6452d
Divinity <69832329+Divine144@users.noreply.github.com>
2023-08-19 22:25:54 -0400

    [publish] made money display on HUD only if you are morph

--- src/main/java/com/divinity/hmedia/rgrbillionaire/event/ClientForgeEvents.java
+++ src/main/java/com/divinity/hmedia/rgrbillionaire/event/ClientForgeEvents.java
@@ -3,9 +3,12 @@ package com.divinity.hmedia.rgrbillionaire.event;
 import com.divinity.hmedia.rgrbillionaire.cap.BillionaireHolderAttacher;
 import com.divinity.hmedia.rgrbillionaire.init.MarkerInit;
 import com.divinity.hmedia.rgrbillionaire.init.MenuInit;
+import com.divinity.hmedia.rgrbillionaire.init.MorphInit;
 import com.divinity.hmedia.rgrbillionaire.network.ClientHandler;
+import com.divinity.hmedia.rgrbillionaire.util.BillionaireUtils;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev._100media.hundredmediaabilities.capability.MarkerHolderAttacher;
+import dev._100media.hundredmediamorphs.capability.MorphHolderAttacher;
 import dev._100media.hundredmediaquests.network.HMQNetworkHandler;
 import dev._100media.hundredmediaquests.network.packet.OpenMainTreePacket;
 import net.minecraft.client.Minecraft;
@@ -47,24 +50,25 @@ public class ClientForgeEvents {
         if (!event.getOverlay().id().equals(FOOD_OVERLAY) || player == null)
             return;
         BillionaireHolderAttacher.getHolder(player).ifPresent(holder -> {
-            // TODO: Check if holder has morph then render
-            var font = Minecraft.getInstance().font;
-            String text = "$%s/$%s".formatted(holder.getMoney(), holder.getMoneyCap());
-            String billionairesClubText = "!! Official Member of the Billionaire’s Club !!";
-            PoseStack poseStack = event.getGuiGraphics().pose();
-            poseStack.pushPose();
-            int x = event.getWindow().getGuiScaledWidth() / 2;
-            int y = event.getWindow().getGuiScaledHeight() / 2 + 70;
-            MarkerHolderAttacher.getMarkerHolder(player).ifPresent(m -> {
-                if (m.hasMarker(MarkerInit.BILLIONAIRES_CLUB.get())) {
-                    event.getGuiGraphics().drawString(font, text, x - (font.width(text) / 2), y - 4, 0x00FF00);
-                    event.getGuiGraphics().drawString(font, billionairesClubText, x - (font.width(billionairesClubText) / 2), y + 8, 0x399A9C);
-                }
-                else {
-                    event.getGuiGraphics().drawString(font, text, x - (font.width(text) / 2), y + 8, 0x00FF00);
-                }
-            });
-            poseStack.popPose();
+            if (BillionaireUtils.hasAnyMorph(player)) {
+                var font = Minecraft.getInstance().font;
+                String text = "$%s/$%s".formatted(holder.getMoney(), holder.getMoneyCap());
+                String billionairesClubText = "!! Official Member of the Billionaire’s Club !!";
+                PoseStack poseStack = event.getGuiGraphics().pose();
+                poseStack.pushPose();
+                int x = event.getWindow().getGuiScaledWidth() / 2;
+                int y = event.getWindow().getGuiScaledHeight() / 2 + 70;
+                MarkerHolderAttacher.getMarkerHolder(player).ifPresent(m -> {
+                    if (m.hasMarker(MarkerInit.BILLIONAIRES_CLUB.get())) {
+                        event.getGuiGraphics().drawString(font, text, x - (font.width(text) / 2), y - 4, 0x00FF00);
+                        event.getGuiGraphics().drawString(font, billionairesClubText, x - (font.width(billionairesClubText) / 2), y + 8, 0x399A9C);
+                    }
+                    else {
+                        event.getGuiGraphics().drawString(font, text, x - (font.width(text) / 2), y + 8, 0x00FF00);
+                    }
+                });
+                poseStack.popPose();
+            }
         });
     }
 }
